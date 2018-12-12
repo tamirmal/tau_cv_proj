7bd5d0c7e0ad2e29a7db357c07133ad272cfccfe
Tamir Malka <tamirmal@gmail.com>
2018-12-12 12:45:30 -0800

    bug fixes

--- SVC_over_CNN/svc_over_cnn.py
+++ SVC_over_CNN/svc_over_cnn.py
@@ -1,7 +1,6 @@
 import numpy as np
 from sklearn.svm import SVC
-from sklearn.cross_validation import StratifiedKFold
-from sklearn.grid_search import GridSearchCV
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
 import pylab as pl
 
 def data_preprocessing(X):
@@ -16,28 +15,28 @@ def train_classifier(X, Y):
     C_range = 10. ** np.arange(-3, 8)
     gamma_range = 10. ** np.arange(-5, 4)
     param_grid = dict(gamma=gamma_range, C=C_range)
-    grid = GridSearchCV(SVC(), param_grid=param_grid, cv=StratifiedKFold(y=Y, k=5))
+    grid = GridSearchCV(SVC(), param_grid=param_grid)
+
+#    import ipdb; ipdb.set_trace()
+
     grid.fit(X, Y)
+    print("The best classifier is: ", grid.best_params_)
+    print("")
     print("The best classifier is: ", grid.best_estimator_)
+
 
     # plot the scores of the grid
     # grid_scores_ contains parameter settings and scores
-    score_dict = grid.grid_scores_
+    score_dict = grid.cv_results_
 
     # We extract just the scores
-    scores = [x[1] for x in score_dict]
-    scores = np.array(scores).reshape(len(C_range), len(gamma_range))
-
-    # Make a nice figure
-    pl.figure(figsize=(8, 6))
-    pl.subplots_adjust(left=0.15, right=0.95, bottom=0.15, top=0.95)
-    pl.imshow(scores, interpolation='nearest', cmap=pl.cm.spectral)
-    pl.xlabel('gamma')
-    pl.ylabel('C')
-    pl.colorbar()
-    pl.xticks(np.arange(len(gamma_range)), gamma_range, rotation=45)
-    pl.yticks(np.arange(len(C_range)), C_range)
-    pl.show()
+    print("Grid scores on training set:")
+    print()
+    means = grid.cv_results_['mean_test_score']
+    stds = grid.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, grid.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r"
+              % (mean, std * 2, params))
 
     return grid.best_estimator_
 
--- train_svm_on_bbox.py
+++ train_svm_on_bbox.py
@@ -1,10 +1,14 @@
-import SVC_over_CNN
-import VGG_feature_extract
+from SVC_over_CNN import svc_over_cnn
+from VGG_feature_extract import vgg_features_extract
 import os
+import numpy as np
+import pickle
 
 
 def main():
-    base_path = '/home/tamir/PycharmProjects/tau_proj_prep/CROPPED'
+#    base_path = '/home/tamirmal/workspace/git/tau_cv_proj'
+    base_path = '/home/tamir/git/tau_cv_proj'
+    base_path_dataset = base_path + '/DATASET'
 
     img_list = []
 
@@ -12,27 +16,32 @@ def main():
         print("Cant locate directory {}".format(base_path))
         exit(-1)
 
-    for cls in os.listdir(base_path):
-        cls_path = base_path + '/' + cls
+    for cls in os.listdir(base_path_dataset):
+        cls_path = base_path_dataset + '/' + cls
         if not os.path.isdir(cls_path):
             print("base_path should contain only directories, {} is not a directory".format(cls))
             exit(-1)
 
-        img_path = cls_path + '/' + img
-        img_suffix = img_path.split('.')[-1]
-        if img_suffix.lower() != '.jpg':
-            print("img path : {} not in jpg format!".format(img_path))
-            exit(-1)
-
         for img in os.listdir(cls_path):
+            img_path = cls_path + '/' + img
+            img_suffix = img_path.split('.')[-1]
+            if img_suffix.lower() != 'jpg':
+                print("img path : {} not in jpg format!".format(img_path))
+                exit(-1)
+
             img_list.append(
                 {
                     'name': cls_path + '/' + img,
                     'class': cls,
                 })
 
-    X, Y = VGG_feature_extract.vgg_prepare_features_for_train(img_list)
-    cls = SVC_over_CNN.train_classifier(X, Y)
+    if True:
+        X, Y = vgg_features_extract.vgg_prepare_features_for_train(img_list)
+        X = np.reshape(X, (len(X), -1))
+
+    cls = svc_over_cnn.train_classifier(X, Y)
+    with open(base_path + '/svm_best_cls.dump') as outf:
+        pickle.dump(cls, outf)
 
 # End of main
 
